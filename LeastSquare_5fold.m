ad8d967af0c31facc5a8499ed22635c827166acd
MadProgrammerGR <giorgozombo@yahoo.gr>
2018-01-20 23:14:52 +0200

    fixed lsqr 5fold

--- LeastSquare_5fold.m
+++ LeastSquare_5fold.m
@@ -2,15 +2,19 @@ clear;
 function [W,SuccessRate] = fold(id)
   data = importdata(strcat("5folds/ff_",num2str(id),".base.csv"), ",", 1).data;
   Y = data(:,3);
+  Y(Y==0) = -1;
   X = data(:,4:end);
   clear data;
   W = inv(X'*X)*X'*Y; %least squares' optimal weights vector of hyperplane
 
   data = importdata(strcat("5folds/ff_",num2str(id),".test.csv"), ",", 1).data;
   Y = data(:,3);
+  Y(Y==0) = -1;
   X = data(:,4:end);
   Guess = W'*X';
-  SuccessRate = 1-sum(abs(round(Guess')-Y))/length(Y);
+  Guess(Guess>0)=1;
+  Guess(Guess<0)=-1;
+  SuccessRate = sum(Guess'==Y)/length(Y);
 endfunction
 
 Wbest = [];
