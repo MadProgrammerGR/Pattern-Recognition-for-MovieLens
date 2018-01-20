afa33aa7ce42b5f9318adeb69ea186f95a051c96
MadProgrammerGR <giorgozombo@yahoo.gr>
2018-01-20 11:13:12 +0200

    added single link algorithm and dendrogram

--- userClustering.m
+++ userClustering.m
@@ -6,7 +6,20 @@ init_repre = rand(18,m)*4+1;
 [km_repre,km_bel] = k_means(X,init_repre);
 printf("k-means cluster size: %d %d\n", sum(km_bel==1), sum(km_bel==2));
 init_repre
-repre
+km_repre
 
-%parallelcoords(X(:,1:100)','Group',km_bel(1:100),'Labels',headers(2:end));
-myParallelCoords(X(:,1:100)',km_bel(1:100),headers(2:end));
+order = randperm(N);
+%parallelcoords(X(:,order(1:100))','Group',km_bel(order(1:100)),'Labels',headers(2:end));
+myParallelCoords(X(:,order(1:100))',km_bel(order(1:100)),headers(2:end));
+
+dista = zeros(N,N);
+for i=1:N
+    for j=i+1:N
+        dista(i,j)=distan(X(:,i),X(:,j));
+        dista(j,i)=dista(i,j);
+    endfor
+endfor
+% oi grammes tou bel einai me th seira oi omadopoihseis pou proekypsan
+[bel,thres]=agglom(dista,1); % 1 for single link, 2 for complete link
+Z=linkage(X','single','euclidean'); % aplou desmou
+figure, dendrogram(Z);
